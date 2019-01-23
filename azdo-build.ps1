aac14172f5e5f3b8af83a01dbbc4d742250b3608
Olivier Miossec <oliviermiossec@hotmail.fr>
2019-01-23 14:56:56 +0100

    Build AzDO

---
+++ azdo-build.ps1
@@ -0,0 +1,18 @@
+# Fill in custom build functions.
+[cmdletbinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [String]
+    $SourceFolder = $Env:BUILD_SOURCESDIRECTORY,
+    [Parameter(Mandatory=$false)]
+    [String]
+    $ModuleName=$ENV:ModuleName
+    )
+
+write-host "Where are we ? : Here $($SourceFolder)"
+
+write-host "Hi, I am your build agent. I run on $($Env:Agent.OS)"
+
+Write-host "I am starting the jon nammed $($Env:Agent.JobName) just for you"
+
+
