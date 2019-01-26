6f3cffefa12dae9458decf1b826f64b16a73ae57
Olivier Miossec <oliviermiossec@hotmail.fr>
2019-01-26 11:14:38 +0100

    Build Script

--- azdo-build.ps1
+++ azdo-build.ps1
@@ -19,8 +19,38 @@ write-host "Hi, I am your build agent. I will build the module $($ModuleName)"
 
 write-host "More info of this task can be found here $($Env:Build_BuildUri)"
 
-write-host "Creating build folder in $($SourceFolder)\build\$($ModuleName)"
 
 $BuildFolder = join-path -path $SourceFolder  -childpath "\Build\$($ModuleName)"
 
+$SourceFolder = join-path -path $SourceFolder  -childpath "\$($ModuleName)"
+
 write-host "Build path $($BuildFolder)"
+
+if (Test-Path $BuildFolder) {
+    Remove-Item -Path $BuildFolder -Force -Recurse -Confirm:$false
+}
+
+new-item -Path $BuildFolder -ItemType Directory
+
+# Creating Modle path
+$BuildModuleFile = Join-Path -Path $BuildFolder -ChildPath "$($ModuleName).psm1"
+$BuildModuleManifest = Join-Path -Path $BuildFolder -ChildPath "$($ModuleName).psd1"
+
+
+Copy-Item -Path (Join-Path -Path $SourceFolder -ChildPath "$($ModuleName)\$($ModuleName).psd1") -Destination $BuildModuleManifest -Force
+
+
+
+$PublicFunctionsList = Get-ChildItem -Path "$($ModuleName)\Public" -Filter *.ps1
+
+$AllFunctions = Get-ChildItem -Path $SourceFolder -Include 'Public', 'External' -Recurse -Directory | Get-ChildItem -Include *.ps1 -File
+
+
+if ($AllFunctions) {
+    Foreach ($Function in $AllFunctions) {
+        Get-Content -Path $AllFunction.FullName | Add-Content -Path $BuildModuleFile
+    }
+}
+
+
+Update-ModuleManifest -Path $BuildModuleManifest -FunctionsToExport $PublicFunctionsList.BaseName
